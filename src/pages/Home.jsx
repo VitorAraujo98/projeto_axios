63e9c297cbf7a0dedc9f8eb38e62d2f4b124ba54
Joao Vitor <joaonikito98@gmail.com>
2023-09-19 10:22:37 -0300

    consumindo api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  
+  const getPosts = async() => {
+    try {
+       const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/1"
+        );
+      const data = response.data;
+      console.log(data);
+
+    } catch (error) {
+      console.log(error)
+      
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+
+   }, [])
+
+ 
   return (
     <div>Home</div>
   )
+ 
 }
+
 
 export default Home
